e93760555cd04eae1c56eb961ca50c327359c7db
Caleb <Calebsirma02@gmail.com>
2024-03-05 00:39:43 +0300

    Initializing test repo

---
+++ Linting.py
@@ -0,0 +1,4 @@
+def test_function():
+	print("This is a test function")
+
+test_function()
---
+++ Linting_test.py
@@ -0,0 +1,5 @@
+import double
+def test_function():
+    assert double.double_int(4)==8
+    assert double.double_string("cat")=="catcat"
+    
